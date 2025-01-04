aa7ffa46636b4bdf8bf8adb584b0cb8197a3d90d
RohanSingh <Rohan.singh04@outlook.com>
2025-01-04 16:22:18 -0500

    updated for modern

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Smooth Scrolling
+// Smooth Scrolling for Navigation
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
